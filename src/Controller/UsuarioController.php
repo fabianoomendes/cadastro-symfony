bb7e8a13f381ad97e032129cb062c3617eb94abe
Fabiano Mendes <fabianomendesoficial@gmail.com>
2022-06-16 12:59:16 -0300

    Update

--- src/Controller/UsuarioController.php
+++ src/Controller/UsuarioController.php
@@ -2,3 +2,27 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/", name="web_usuario_")
+ */
+class UsuarioController
+{
+    /**
+     * @Route("/", methods={"GET"}, name="index")
+     */
+    public function index(): Response
+    {
+        return new Response("Implementar formulário de cadastro", 200);
+    }
+
+    /**
+     * @Route("/salvar", methods={"POST"}, name="salvar")
+     */
+    public function salvar(): Response
+    {
+        return new Response("Imprementar gravação ao banco de dados", 200);
+    }
+}
