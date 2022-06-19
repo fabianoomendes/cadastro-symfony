aabb8df9ab2a02136b8baaa6f2af1451971502d3
Fabiano Mendes <fabianomendesoficial@gmail.com>
2022-06-19 01:44:56 -0300

    Update

--- src/Controller/UsuarioController.php
+++ src/Controller/UsuarioController.php
@@ -2,20 +2,22 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * @Route("/", name="web_usuario_")
  */
-class UsuarioController
+class UsuarioController extends AbstractController
 {
     /**
      * @Route("/", methods={"GET"}, name="index")
      */
     public function index(): Response
     {
-        return new Response("Implementar formulário de cadastro", 200);
+        // return $this->render("usuario/form.html.twig");
+        return $this->render("usuario/erro.html.twig", ['fulano' => "Adriano"]);
     }
 
     /**
